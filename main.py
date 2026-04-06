735f36a703c0e78549be7d10a3d870fcbb145e76
Dexter1210 <deveshsharma1210@gmail.com>
2026-04-06 13:51:42 +0530

    Changes to integrate with frontend

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from database import engine, Base
 from routers import users, tasks
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Task Manager")
+
+app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:3000"],
+    allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 @app.get("/")
 def read_root():
